5d1be9c35f2b6209a93c7166caa58c62957355b4
Clement Caira <clement.caira@supinternet.fr>
2017-11-30 16:27:54 +0100

    improve serveur 404 not found

--- serveur.py
+++ serveur.py
@@ -1,16 +1,39 @@
-import aiohttp
+from http.server import BaseHTTPRequestHandler
+import argparse
 import asyncio
-import async_timeout
+import sys
+from aiohttp import web
 
-async def fetch(session, url):
-    with async_timeout.timeout(10):
-        async with session.get(url) as response:
-            return await response.text()
+async def run_command(*args):
+    process = await asyncio.create_subprocess_exec(
+        *args,
+        stdout=asyncio.subprocess.PIPE)
+    stdout, stderr = await process.communicate()
+    return stdout.decode().strip()
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://python.org')
-        print(html)
+async def handle(request):
+    ip = request.match_info.get('ip', "")
+    if ip == "":
+        return ip
+    else:
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+        commands = run_command('nmap', '-sV', ip)
+        result = await commands
+        text = str(format(result))
+    return web.Response(text=text)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port")
+
+    parser.add_argument("--host")
+
+    args = parser.parse_args()
+
+    print(int(args.port), args.host)
+    app = web.Application()
+    app.router.add_get('/{ip}', handle)
+
+    web.run_app(app, host=args.host, port=int(args.port))
+
+main()
